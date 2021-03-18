8f8b8bb33eba586e91853dfa0a46d22ecb19480e
Lucas Angeli <lucasangeli819@gmail.com>
2021-03-18 16:04:35 -0300

    Add files via upload

---
+++ force-app/main/default/aura/expenses/expensesController.js
@@ -0,0 +1,28 @@
+({
+    clickCreate: function(component, event, helper) {
+        let validExpense = component.find('expenseform').reduce(function (validSoFar, inputCmp) {
+            inputCmp.showHelpMessageIfInvalid();
+            return validSoFar && inputCmp.get('v.validity').valid;
+        }, true);
+
+        if(validExpense){
+            let newExpense = component.get("v.newExpense");
+            console.log("Create expense: " + JSON.stringify(newExpense));
+            helper.createExpense(component, newExpense);
+        }
+    },
+
+    doInit: function(component, event, helper) {
+        let action = component.get("c.getExpenses");d
+        action.setCallback(this, function(response) {
+            let state = response.getState();
+            if (state === "SUCCESS") {
+                component.set("v.expenses", response.getReturnValue());
+            }
+            else {
+                console.log("Failed with state: " + state);
+            }
+        });
+        $A.enqueueAction(action);
+    },
+})
